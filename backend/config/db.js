f2446e035ca51e43c9798948a3c10a31e83cfa2c
unknown <muthahar85220@gmail.com>
2025-05-27 10:27:13 +0530

    bugfix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function connectDB() {
    try {
-    await mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/ecom-database");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database is connected successfully");
    } catch(error) {
     console.error("Failed to connect Databse", error)
