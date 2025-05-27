d96323ca60baf1759da83de8d5b46c2684a29da5
unknown <muthahar85220@gmail.com>
2025-05-27 14:43:16 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite default port
+  origin: ['http://localhost:5173', 'https://muthaharsmart.netlify.app'],
   credentials: true // if using cookies/auth
 }));
 
